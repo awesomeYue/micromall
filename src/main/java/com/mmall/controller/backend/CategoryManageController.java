d003ba68e52b2330466eb65795a056b57738aefe
suny <sy2527552@gmail.com>
2020-05-13 16:51:10 +0800

    分类管理接口的å实现

---
+++ src/main/java/com/mmall/controller/backend/CategoryManageController.java
@@ -0,0 +1,99 @@
+package com.mmall.controller.backend;
+
+import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Category;
+import com.mmall.pojo.User;
+import com.mmall.services.CategoryService;
+import com.mmall.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
+@RequestMapping("/manage/category")
+public class CategoryManageController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @RequestMapping(value = "add_category.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse addCateogry(HttpSession session,String categoryName,@RequestParam(value = "parentId",defaultValue = "0") int parentId){
+        User user=(User)session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+           return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录，请登录");
+        }
+        //校验一下是否为管理员登录
+        if (userService.checkAdminRole(user).isSuccess()) {
+            //是管理员
+            //增加我们处理分类的逻辑
+            return categoryService.addCategory(categoryName,parentId);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+    @RequestMapping(value = "set_category.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse updateCategory(HttpSession session,String categoryName, int parentId){
+        User user=(User)session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录，请登录");
+        }
+        //校验一下是否为管理员登录
+        if (userService.checkAdminRole(user).isSuccess()) {
+            //是管理员
+            //增加我们处理分类的逻辑
+            return categoryService.updateCategory(categoryName,parentId);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+
+    @RequestMapping(value = "get_category.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<List<Category>> getChildrenParallelCategory(HttpSession session, @RequestParam(value = "categoryId",defaultValue = "0") Integer categoryId){
+        User user=(User)session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录，请登录");
+        }
+        //校验一下是否为管理员登录
+        if (userService.checkAdminRole(user).isSuccess()) {
+            //是管理员
+            //查询字节点的category的信息，并且不递归，保持平级
+            return categoryService.getChildrenParallelCategory(categoryId);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+    @RequestMapping(value = "get_deep_category.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse getCategoryAndRecursionChildrenCategory(HttpSession session,@RequestParam(value="categoryId",defaultValue = "0")Integer categoryId){
+        User user=(User)session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录，请登录");
+        }
+        //校验一下是否为管理员登录
+        if (userService.checkAdminRole(user).isSuccess()) {
+            //是管理员
+            //查询当前节点的id以及递归字节点的
+            return categoryService.selectCategoryAndChildrenById(categoryId);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+}
---
+++ src/main/java/com/mmall/services/impl/CategoryServiceImpl.java
@@ -0,0 +1,95 @@
+package com.mmall.services.impl;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.mmall.common.ServerResponse;
+import com.mmall.dao.CategoryMapper;
+import com.mmall.pojo.Category;
+import com.mmall.services.CategoryService;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+@Service("categoryService")
+public class CategoryServiceImpl implements CategoryService {
+
+    private static final Logger logger = LoggerFactory.getLogger(CategoryServiceImpl.class);
+
+    @Autowired
+    CategoryMapper categoryMapper;
+
+    @Override
+    public ServerResponse addCategory(String cateName, Integer parentId) {
+        if (parentId == null || StringUtils.isBlank(cateName)) {
+            return ServerResponse.createByErrorMessage("添加品类参数错误！");
+        }
+        Category category=new Category();
+        category.setParentId(parentId);
+        category.setName(cateName);
+        category.setStatus(true);
+        int row=categoryMapper.insert(category);
+        if(row>0) {
+            return ServerResponse.createBySuccessMessage("添加品类成功");
+        }
+        return ServerResponse.createByErrorMessage("添加品类失败");
+    }
+
+    @Override
+    public ServerResponse updateCategory(String cateName, Integer parentId) {
+        if (parentId == null || StringUtils.isBlank(cateName)) {
+            return ServerResponse.createByErrorMessage("添加品类参数错误！");
+        }
+        Category category=new Category();
+        category.setParentId(parentId);
+        category.setName(cateName);
+        category.setUpdateTime(new Date());
+        int row=categoryMapper.updateByPrimaryKeySelective(category);
+        if(row>0) {
+            return ServerResponse.createBySuccessMessage("更新品类成功");
+        }
+        return ServerResponse.createByErrorMessage("更新品类失败");
+    }
+
+    @Override
+    public ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId) {
+        List<Category> list=categoryMapper.selectChildrenParallelCategory(categoryId);
+        if (CollectionUtils.isEmpty(list)) {
+            logger.info("查询的品类为空");
+        }
+        return ServerResponse.createBySuccess(list);
+    }
+
+    @Override
+    public ServerResponse selectCategoryAndChildrenById(Integer categoryId){
+        Set<Category> categorySet= Sets.newHashSet();
+        findChildCategory(categorySet,categoryId);
+        List<Integer>categoryIdList=Lists.newArrayList();
+        if (categoryId == null) {
+            for (Category categoryItem : categorySet) {
+                categoryIdList.add(categoryItem.getId());
+            }
+        }
+        return ServerResponse.createBySuccess(categoryIdList);
+    }
+
+    //递归算法，算出字节点
+    private Set<Category> findChildCategory(Set<Category> categorySet,Integer categoryId){
+        Category category=categoryMapper.selectByPrimaryKey(categoryId);
+        if (category != null) {
+            categorySet.add(category);
+        }
+        //查找字节点，递归算法一定要有一个退出机制
+        List<Category> categoryList=categoryMapper.selectChildrenParallelCategory(categoryId);
+        for (Category categoryItem : categoryList) {
+            findChildCategory(categorySet,categoryItem.getId());
+        }
+        return categorySet;
+    }
+}
